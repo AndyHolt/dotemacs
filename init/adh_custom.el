9cf80d8d3b81a1ead0fc4f58afa3cd0d41350d6c
adh635 <andrew.holt@hotmail.co.uk>
2013-03-06 00:12:45 +0000

    Added capture for Org

--- init/adh_custom.el
+++ init/adh_custom.el
@@ -2,8 +2,9 @@
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won'adh_t work right.
- '(custom-safe-themes (quote ("71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default))))
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default)))
+ '(org-agenda-files (quote ("~/Dropbox/Org_files/Work.org" "~/Dropbox/Org_files/todo.org" "~/Dropbox/Org_files/CICCU_Treasurer.org" "~/Dropbox/Org_files/CICCU_Exec.org" "~/Dropbox/Org_files/hpcomm.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
