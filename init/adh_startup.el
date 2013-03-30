3adc2c9cc49406381cae6efd172a2edd90192a49
Andy <andrew.holt@hotmail.co.uk>
2013-03-30 00:44:28 +0000

    Added windows specific customisations

--- init/adh_startup.el
+++ init/adh_startup.el
@@ -1,5 +1,10 @@
 ;; things to do when starting emacs
 
-(find-file "~/Dropbox/Org_files/todo.org")
+(defvar adh-dropbox-location
+  (cond ((eq system-type 'gnu-linux) "~/Dropbox/")
+	((eq system-type 'windows-nt) "d:/Dropbox/"))
+  "Holds the location of the Dropbox root directory for the current system")
+
+(find-file (concat adh-dropbox-location "Org_files/todo.org"))
 
 (provide 'adh_startup)
