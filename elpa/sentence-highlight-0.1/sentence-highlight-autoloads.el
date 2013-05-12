cbe34e0d90c68bc708804e559720b61d261213c0
adh635 <andrew.holt@hotmail.co.uk>
2013-05-12 17:37:44 +0100

    installed packeges

---
+++ elpa/sentence-highlight-0.1/sentence-highlight-autoloads.el
@@ -0,0 +1,18 @@
+;;; sentence-highlight-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("sentence-highlight-pkg.el" "sentence-highlight.el")
+;;;;;;  (20846 52491 563388))
+
+;;;***
+
+(provide 'sentence-highlight-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sentence-highlight-autoloads.el ends here
