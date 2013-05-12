396bbf86593011dedaaee9c5d58da0330d040d5f
adh635 <andrew.holt@hotmail.co.uk>
2013-05-12 22:24:32 +0100

    remove acejump file (now in navigation)

--- init/adh_acejump.el
+++
@@ -1,9 +0,0 @@
-(require 'ace-jump-mode)
-
-;;; setup keyboard shortcuts for ace-jump mode
-
-(global-set-key "\C-cj" 'ace-jump-mode)
-(global-set-key "\C-ck" 'ace-jump-mode-pop-mark)
-(global-set-key "\C-cw" 'ace-jump-word-mode)
-
-(provide 'adh_acejump)
