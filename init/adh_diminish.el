505b784e36294ebeb45eacd8bd9aa8387542bd6c
Andy Holt <andrew.holt@hotmail.co.uk>
2013-05-27 20:55:35 +0100

    diminish mode setup

--- init/adh_diminish.el
+++ init/adh_diminish.el
@@ -1,10 +1,16 @@
 ;; setup mode line to display less about minor modes
 (require 'diminish)
 
-(diminish 'abbrev-mode "α")
-(diminish 'yas-minor-mode "γ")
-(diminish 'auto-fill-function "f")
-(diminish 'flyspell-mode "fs")
-(diminish 'org-indent-mode "i")
+(when (require 'diminish nil 'noerror)
+  (eval-after-load "abbrev"
+    (diminish 'abbrev-mode "Ab"))
+  (eval-after-load "yasnippet"
+    (diminish 'yas-minor-mode "ys"))
+  (eval-after-load "flyspell"
+    (diminish 'flyspell-mode "Fl")))
+
+(add-hook 'emacs-lisp-mode-hook
+	  (lambda ()
+	    (setq mode-name "el")))
 
 (provide 'adh_diminish)
