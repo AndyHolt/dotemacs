4e0d4d44ba8d9fc6e17d4c786f22c83f7559cefb
Andy Holt <andrew.holt@hotmail.co.uk>
2013-11-05 15:41:10 +0000

    Updated packages

---
+++ elpa/keyfreq-20131012.2138/keyfreq-pkg.el
@@ -0,0 +1,1 @@
+(define-package "keyfreq" "20131012.2138" "track command frequencies [github]" (quote nil))
---
+++ elpa/sentence-highlight-20121026.1450/sentence-highlight-autoloads.el
@@ -0,0 +1,18 @@
+;;; sentence-highlight-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("sentence-highlight-pkg.el" "sentence-highlight.el")
+;;;;;;  (21113 4285 217351))
+
+;;;***
+
+(provide 'sentence-highlight-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sentence-highlight-autoloads.el ends here
