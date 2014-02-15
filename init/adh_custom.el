b01b7d19a3cce3203e716ea7b14d6870da23de10
Andy Holt <andrew.holt@hotmail.co.uk>
2014-02-15 21:25:55 +0000

    Something changed in custom file.

--- init/adh_custom.el
+++ init/adh_custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default)))
- '(nil nil t))
+ '(nil nil t)
+ '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
