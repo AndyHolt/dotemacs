40ec764cc460332d5771e67e9fa95a8d7daf1afe
Andy Holt <andrew.holt@hotmail.co.uk>
2014-02-25 16:10:38 +0000

    Change to inconsolata font, just for a change.

--- init/adh_gui.el
+++ init/adh_gui.el
@@ -29,7 +29,7 @@
 ;; Font selection - platform dependant
 ;;   Updated to work with server mode operation.
 (cond ((eq system-type 'gnu/linux)
-       (setq default-frame-alist '((font . "Monaco-11")
+       (setq default-frame-alist '((font . "Inconsolata-12")
                                    (background-mode . dark))))
       ((eq system-type 'windows-nt)
        (setq default-frame-alist '((font . "Inconsolata-12")
