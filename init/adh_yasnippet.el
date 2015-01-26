b4b85a71201014fd06d1ec03342ffc199db90a78
Andy Holt <andrew.holt@hotmail.co.uk>
2015-01-26 22:02:47 +0000

    Changes to yasnippet setup.
    Use helm as default snippet selector.
    Change todo snippet to use a more obvious name.
    Don't load packaged snippets, just my ones.

--- init/adh_yasnippet.el
+++ init/adh_yasnippet.el
@@ -15,6 +15,9 @@
 
 (require 'yasnippet)
 
+;; set directories for snippets to be found
+(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
+
 (yas-global-mode 1)
 ; taken directly from customize file - need to make work...
 ; can't get this to work - for now being done from custom.el file, written by customize
@@ -30,6 +33,31 @@
 
 ;; reactivate snippet fields on undo/redo
 (setq yas-snippet-revival t)
+
+;; use helm to select from multiple possible snippets.
+(defun adh-yas-helm-prompt (prompt choices &optional display-fn)
+    "Use helm to select a snippet. Put this into `yas-prompt-functions'."
+  (interactive)
+  (setq display-fn (or display-fn 'identity))
+  (if (require 'helm-config)
+      (let (tmpsource cands result rmap)
+        (setq cands (mapcar (lambda (x) (funcall display-fn x)) choices))
+        (setq rmap (mapcar (lambda (x) (cons (funcall display-fn x) x))
+                           choices))
+        (setq tmpsource
+              (list
+               (cons 'name prompt)
+               (cons 'candidates cands)
+               '(action . (("Expand" . (lambda (selection) selection))))
+               ))
+        (setq result (helm-other-buffer '(tmpsource) "*helm-select-yasnippet"))
+        (if (null result)
+            (signal 'quit "user quit!")
+          (cdf (assoc result rmap))))
+    nil))
+
+(setq yas-prompt-functions '(shk-yas/helm-prompt yas-ido-prompt yas-no-prompt))
+
 
 (provide 'adh_yasnippet)
 
