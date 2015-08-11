c18c171de90f3ad1ad6bc7efa8c52db222a3c7d5
Andy Holt <andrew.holt@hotmail.co.uk>
2015-08-11 13:35:43 +0100

    Config for deft and hydra.

---
+++ init/adh_deft.el
@@ -0,0 +1,21 @@
+;;; adh_deft.el --- Customizations for deft mode.
+
+;; Author: Andy Holt (andrew.holt@hotmail.co.uk)
+;; Date: Tue 26 May 2015 19:55
+
+;;; Commentary:
+;;
+;; Customisations for Deft (http://jblevins.org/projects/deft/)
+
+;;; Code:
+
+(setq deft-extension "md")
+(setq deft-directory "~/Dropbox/Notes")
+(setq deft-text-mode 'markdown-mode)
+
+;; use file name rather than first line as note title
+(setq deft-use-filename-as-title t)
+
+(provide 'adh_deft)
+
+;;; adh_deft.el ends here
---
+++ init/adh_hydra.el
@@ -0,0 +1,52 @@
+;;; adh_hydra.el --- set up hydra package - keybindings that stick around
+
+;; Author: Andy Holt (andrew.holt@hotmail.co.uk)
+;; Date: Sat 30 May 2015 12:05
+
+;;; Commentary:
+;;
+;; Hydra provides a modal element to emacs, which allows use of repeated
+;; keybindings more easily.
+
+;;; Code:
+
+(defun hydra-universal-arugment (arg)
+  (interactive "P")
+  (setq prefix-arg (if (consp arg)
+                       (list (* 4 (car arg)))
+                     (if (eq arg '-)
+                         (list -4)
+                       '(4)))))
+
+(global-set-key
+ (kbd "C-M-o")
+ (defhydra hydra-window (:color amaranth)
+   "window"
+   ("h" windmove-left "left")
+   ("j" windmove-down "down")
+   ("k" windmove-up "up")
+   ("l" windmove-right "right")
+   ("v" (lambda ()
+          (interactive)
+          (split-window-right)
+          (windmove-right))
+        "vert")
+   ("x" (lambda ()
+          (interactive)
+          (split-window-below)
+          (windmove-down))
+        "horiz")
+   ("t" transpose-frame "transpose")
+   ("o" delete-other-windows "one" :color blue)
+   ("a" ace-window "ace")
+   ("s" ace-swap-window "swap")
+   ("d" ace-delete-window "del")
+   ("i" ace-maximise-window "ace-one" :color blue)
+   ("b" ido-switch-buffer "buf")
+   ("m" headlong-bookmark-jump "bmk")
+   ("q" nil "cancel")))
+        
+
+(provide 'adh_hydra)
+
+;;; adh_hydra.el ends here
