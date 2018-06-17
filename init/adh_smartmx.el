410fbe3013054641af0207002f2bb95accdd4414
Andy Holt <andrew.holt@hotmail.co.uk>
2018-06-17 18:00:53 +0100

    smartmx setup

--- init/adh_smartmx.el
+++ init/adh_smartmx.el
@@ -11,13 +11,13 @@
 
 ;;; Code:
 
-;(require 'smex)
+(require 'smex)
 (smex-initialize)
 
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;(global-set-key (kbd "M-x") 'smex)
+;(global-set-key (kbd "M-X") 'smex-major-mode-commands)
 ;; old M-x
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+; (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 (provide 'adh_smartmx)
 
