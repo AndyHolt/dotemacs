5f716df0cc2d4d5836eb8908cd5e21367c297aba
Andy Holt <andrew.holt@hotmail.co.uk>
2019-10-02 15:20:43 -0400

    Show org-habit in agenda even when completed.

--- init/adh_org.el
+++ init/adh_org.el
@@ -674,11 +674,14 @@ When you're done editing press `\\[adh-edit-quote-finalize]' to continue.")))
 
 (define-key adh-edit-quote-mode-map "\C-c\C-c" 'adh-edit-quote-finalize)
 
-
 ;; in org headlines, jump to start and end of headline text with a single C-a or
 ;; C-e. To get to the true start of line and end of line (before stars and todo
 ;; status, or after tags), simply press again.
 (setq org-special-ctrl-a/e t)
+
+;; org-habit view in agenda
+(setq org-habit-show-all-today t
+      org-habit-show-done-always-green t)
 
 (provide 'adh_org)
 
