d58a1b7113411b943b71efb7a8164b451621c29b
Andy Holt <andrew.holt@hotmail.co.uk>
2020-06-26 21:59:03 +0100

    Fix loading of org-id for using ID properties

--- init/adh_org.el
+++ init/adh_org.el
@@ -870,6 +870,7 @@ exporting, replace them for normal usage."
 ;; 2020-04-06: I think this is the behaviour I want, always use ID if possible,
 ;; but some org files or headings may be best without IDs, so don't force
 ;; creation of an ID if creating a link to them.
+(require 'org-id)
 (setq org-id-link-to-org-use-id t)
 
 (require 'org-zett)
