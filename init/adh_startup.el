b3c505c5b98b8c3f57a46139a24039f7b45d2973
Andy Holt <andrew.holt@hotmail.co.uk>
2020-06-28 21:35:39 +0100

    Add function to display emacs start up time

--- init/adh_startup.el
+++ init/adh_startup.el
@@ -54,5 +54,18 @@
 ;; don't show an initial startup message
 (setq initial-scratch-message nil)
 
+;; display startup time information
+;; mainly for comparison of startup time with my config files compared with a
+;; clean emacs instance.
+;; See https://blog.d46.us/advanced-emacs-startup/
+;; use a hook so that the message doesn't get clobbered by other messages
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+
 (provide 'adh_startup)
 ;;; adh_startup.el ends here
