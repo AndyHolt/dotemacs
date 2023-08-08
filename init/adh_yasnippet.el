07e806cca727f69e96bff6e6815e061288daf737
Andy Holt <andrew.holt@hotmail.co.uk>
2023-08-08 22:39:55 +0100

    Remove dependency on deprecated helm-config file

--- init/adh_yasnippet.el
+++ init/adh_yasnippet.el
@@ -36,7 +36,7 @@
     "Use helm to select a snippet. Put this into `yas-prompt-functions'."
   (interactive)
   (setq display-fn (or display-fn 'identity))
-  (if (require 'helm-config)
+  ;; (if (require 'helm-config)
       (let (tmpsource cands result rmap)
         (setq cands (mapcar (lambda (x) (funcall display-fn x)) choices))
         (setq rmap (mapcar (lambda (x) (cons (funcall display-fn x) x))
@@ -51,7 +51,8 @@
         (if (null result)
             (signal 'quit "user quit!")
           (cdr (assoc result rmap))))
-    nil))
+    ;; nil)
+  )
 
 (setq yas-prompt-functions '(adh-yas-helm-prompt yas-ido-prompt yas-no-prompt))
 
